7bb6b1e952e9481dc93b2037b6e80ee18d4b58ca
Bruno Tomé <ibrunotome@gmail.com>
2016-11-16 21:57:17 -0200

    Fix the block in solicited format.

--- simturing.py
+++ simturing.py
@@ -6,8 +6,8 @@ def header():
     Return the header for this work
     """
     header_string = '\nSimulador de Máquina de Turing ver 1.0'
-    header_string += '\nDesenvolvido como trabalho prático para a disciplina de Teoria da Computação'
-    header_string += '\nAutores: Bruno Tomé, Ronan Nunes'
+    header_string += '\nDesenvolvido como trabalho prático para a disciplina de Teoria da Computação.'
+    header_string += '\nAutores: Bruno Tomé, Ronan Nunes.'
     header_string += '\nIFMG, 2016.'
 
     return header_string
@@ -25,6 +25,19 @@ def get_block(_line):
     return block
 
 
+def fix_block_size(_block):
+    """
+    Fix the block in solicited format
+    :param _block:
+    :return:
+    """
+    block_fixed = ''
+    for i in range(0, (16 - len(_block))):
+        block_fixed += '.'
+
+    return block_fixed + _block
+
+
 # initialWord = raw_input('Forneça a palavra inicial: ')
 #
 # print initialWord
@@ -38,4 +51,5 @@ if __name__ == '__main__':
         if line.startswith(';'):
             continue
         if line.startswith('bloco'):
-            print get_block(line)
+            block = get_block(line)
+            print fix_block_size(block)
