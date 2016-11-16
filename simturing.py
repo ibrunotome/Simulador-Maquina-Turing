7207542e7966038fa311e495ed5a509357268e59
Bruno Tomé <ibrunotome@gmail.com>
2016-11-16 20:53:51 -0200

    Initial commit.

---
+++ simturing.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+print 'Simulador de Máquina de Turing ver 1.0'
+print 'Desenvolvido como trabalho prático para a disciplina de Teoria da Computação'
+print 'Autores: Bruno Tomé, Ronan Nunes'
+print 'IFMG, 2016.'
+
+initialWord = raw_input('Forneça a palavra inicial: ')
+
+print initialWord
