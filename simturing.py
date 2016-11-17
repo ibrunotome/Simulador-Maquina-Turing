c6bb9d86837309099da4dddb2423f45e2ec94d12
Bruno Tomé <ibrunotome@gmail.com>
2016-11-16 22:34:44 -0200

    Get current and next state of non block.

--- simturing.py
+++ simturing.py
@@ -8,7 +8,7 @@ def header():
     header_string = '\nSimulador de Máquina de Turing ver 1.0'
     header_string += '\nDesenvolvido como trabalho prático para a disciplina de Teoria da Computação.'
     header_string += '\nAutores: Bruno Tomé, Ronan Nunes.'
-    header_string += '\nIFMG, 2016.'
+    header_string += '\nIFMG, 2016.\n'
 
     return header_string
 
@@ -18,24 +18,79 @@ def get_block(_line):
     Get the block of current line
 
     :param _line:
-    :return:
+    :return str:
     """
-    block = _line.split()
-    block = block[1]
-    return block
+    _block = _line.split()
+    _block = _block[1]
+
+    return _block
 
 
-def fix_block_size(_block):
+def fix_block_output(_block):
     """
-    Fix the block in solicited format
+    Fix the block output in solicited format
+
     :param _block:
-    :return:
+    :return str:
     """
-    block_fixed = ''
+    _block_fixed = ''
     for i in range(0, (16 - len(_block))):
-        block_fixed += '.'
+        _block_fixed += '.'
 
-    return block_fixed + _block
+    return _block_fixed + _block
+
+
+def get_initial_block_state(_line):
+    """
+    Get the initial state of block
+
+    :param _line:
+    :return str:
+    """
+    _initial_state = _line.split()
+    _initial_state = _initial_state[2]
+
+    return _initial_state
+
+
+def fix_state_output(_state):
+    """
+    Fix the state output in the solicited format
+
+    :param _state:
+    :return str:
+    """
+    _state_fixed = ''
+    for i in range(0, (4 - len(_state))):
+        _state_fixed += '0'
+
+    return _state_fixed + _state
+
+
+def get_current_state(_line):
+    """
+    Get the current state of non block line
+
+    :param _line:
+    :return str:
+    """
+    _state = _line.split()
+    _state = _state[0]
+
+    return _state
+
+
+def get_next_state(_line):
+    """
+    Get the next state of non block line
+
+    :param _line:
+    :return str:
+    """
+    _state = _line.split()
+    _state = _state[5]
+
+    return _state
 
 
 # initialWord = raw_input('Forneça a palavra inicial: ')
@@ -48,8 +103,20 @@ if __name__ == '__main__':
     script = open('palindromo.MT', 'r')
 
     for line in script:
+        # Jump comments
         if line.startswith(';'):
             continue
+
+        # Check if there is a block
         if line.startswith('bloco'):
             block = get_block(line)
-            print fix_block_size(block)
+            block_output = fix_block_output(block) + '.'
+            state = get_initial_block_state(line)
+            state_output = fix_state_output(state)
+            # print block_output + state_output
+
+        if line.startswith(' '):
+            current_state = get_current_state(line)
+            next_state = get_next_state(line)
+            print fix_state_output(current_state) + ' ' + fix_state_output(next_state)
+            exit(0)
